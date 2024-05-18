676750d4827fcaeb6fa6b73aa4475869d43e6661
Raftouch <rafanelkin@gmail.com>
2024-05-18 17:23:01 +0200

    add counter

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -8,8 +8,34 @@ class CounterPage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<CounterPage> {
+  // variable
+  int _counter = 0;
+
+  // method
+  void _incrementCounter() {
+    // rebuild the widget
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // UI
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // msg
+          Text('You pushed the button this many times'),
+          // counter value
+          Text(_counter.toString(), style: TextStyle(fontSize: 40)),
+          // button
+          ElevatedButton(onPressed: _incrementCounter, child: Text('Increment'))
+        ],
+      ),
+    ));
   }
 }
