b57672d5e1dbac279d0fc43f54174d647f21405f
Raftouch <rafanelkin@gmail.com>
2024-05-18 17:56:42 +0200

    convert text page from stateless to stateful

--- lib/pages/text_page.dart
+++ lib/pages/text_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class TextPage extends StatelessWidget {
+class TextPage extends StatefulWidget {
   const TextPage({super.key});
 
+  @override
+  State<TextPage> createState() => _TextPageState();
+}
+
+class _TextPageState extends State<TextPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
