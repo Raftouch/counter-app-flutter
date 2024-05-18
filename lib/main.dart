552389c9e9231dd34bddb9157171c3f41b314f05
Raftouch <rafanelkin@gmail.com>
2024-05-18 17:24:05 +0200

    remove show debug

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: CounterPage()
-    );
+        debugShowCheckedModeBanner: false, home: CounterPage());
   }
 }
